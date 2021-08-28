50a8f6e43585b6268019a8125f7de61157981d5a
Oger <egor.katlinskii@yandex.by>
2021-08-28 16:21:24 +0300

    init commit

---
+++ HashCat/src/main/java/com/example/hashcat/HashCatApplication.java
@@ -0,0 +1,13 @@
+package com.example.hashcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashCatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HashCatApplication.class, args);
+    }
+
+}
